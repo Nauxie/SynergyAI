7a0f3b14b072209519d2cb803aefe0ee2956c69c
mnj <mgntprg@gmail.com>
2019-06-25 21:10:51 -0700

    Rutput.js

---
+++ pyelements/output.js
@@ -0,0 +1,584 @@
+{
+	name:  Anti-Mage 
+	type1:   Elusive 
+	type2:   Demon Hunter 
+	type3: : 500/1000/2000
+	Health:   42/83/167 
+	DPS:   1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Axe 
+	type1:   Brawny 
+	type2:   Warrior 
+	type3:  700/1400/2800
+	Health:   35/70/140 
+	DPS:   1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Batrider 
+	type1:   Troll 
+	type2:   Knight 
+	type3:  500/1000/2000
+	Health:   37/73/146 
+	DPS:   3/3/3 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Bloodseeker 
+	type1:   Human 
+	type2:   Assassin 
+	type3:  500/1000/2000
+	Health:   29/65/103 
+	DPS:   1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Bounty Hunter 
+	type1:   Scrappy 
+	type2:   Assassin 
+	type3:  550/1100/2200
+	Health:   52/105/210 
+	DPS:   1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Clockwerk 
+	type1:   Scrappy 
+	type2:   Inventor 
+	type3:  700/1400/2800
+	Health:   36/71/143 
+	DPS:   1/1/1 
+	Attack_Range:   5/7/9 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Drow Ranger 
+	type1:   Heartless 
+	type2:   Hunter 
+	type3:  400/800/1600
+	Health:   37/73/146 
+	DPS:   4/4/4 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Enchantress 
+	type1:   Savage 
+	type2:   Druid 
+	type3:  400/800/1500
+	Health:   43/80/153 
+	DPS:   2/2/2 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Ogre Magi 
+	type1:   Blood-Bound 
+	type2:   Mage 
+	type3:  800/1600/3000
+	Health:   43/86/171 
+	DPS:   1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Shadow Shaman 
+	type1:   Troll 
+	type2:   Shaman 
+	type3:  550/1100/2200
+	Health:  28/56/112 
+	DPS:   3/3/3 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Tinker 
+	type1:   Scrappy 
+	type2:   Inventor 
+	type3:  500/1000/2000
+	Health:   30/60/120 
+	DPS:   4/4/4 
+	Attack_Range:   10/10/10 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Tiny 
+	type1:   Primordial 
+	type2:   Warrior 
+	type3:  650/1300/2600
+	Health:   42/84/168 
+	DPS:   1/1/1 
+	Attack_Range:  5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Tusk 
+	type1:   Savage 
+	type2:   Warrior 
+	type3:  650/1300/2600
+	Health:   44/88/175 
+	DPS:   1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Warlock 
+	type1:  Blood-Bound 
+	type2:  Warlock 
+	Health:  650/1450/2850 
+	DPS:   29/59/112 
+	Attack_Range:   4/4/4 
+	Armor:  0/0/0 
+}, 
+{
+	name:  Beastmaster 
+	type1:  Brawny 
+	type2:  Hunter 
+	type3:  600/1200/2400
+	Health:   59/118/236 
+	DPS:   1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Chaos Knight 
+	type1:   Demon 
+	type2:   Knight 
+	type3:  750/1500/3000
+	Health:   58/115/231 
+	DPS:   1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:  0/0/0 
+}, 
+{
+	name:  Crystal Maiden 
+	type1:   Human 
+	type2:  Mage 
+	type3:  450/900/1800
+	Health:   25/50/100 
+	DPS:   4/4/4 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Juggernaut 
+	type1:   Brawny 
+	type2:   Warrior 
+	type3:  600/1200/2400
+	Health:  61/123/245 
+	DPS:  1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Luna 
+	type1:   Elusive 
+	type2:   Knight 
+	Health:  500/1000/2000 
+	DPS:   42/85/169 
+	Attack_Range:  3/3/3 
+	Armor:  5/5/5 
+}, 
+{
+	name:  Morphling 
+	type1:  Primordial 
+	type2:   Assassin 
+	type3:  500/1000/2000
+	Health:   40/81/162 
+	DPS:   3/3/3 
+	Attack_Range:   5/5/5 
+	Armor:  : 0/0/0 
+}, 
+{
+	name:  Nature’s Prophet 
+	type1:   Elusive 
+	type2:   Druid 
+	type3:  500/1000/2000
+	Health:   34/68/136 
+	DPS:   3/3/3 
+	Attack_Range:   0/0/0 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Puck 
+	type1:  : Elusive 
+	type2:   Dragon 
+	type3: Mage
+	Health:   450/900/1800 
+	DPS:   26/53/106 
+	Attack_Range:   3/3/3 
+	Armor:   5/5/5 
+}, 
+{
+	name:  Pudge 
+	type1:   Heartless 
+	type2:   Warrior 
+	Health:  1000/2500/5500 
+	DPS:   29/59/88 
+	Attack_Range:   1/1/1 
+	Armor:   5/5/5 
+}, 
+{
+	name:  Queen of Pain 
+	type1:   Demon 
+	type2:   Assassin 
+	type3:  550/1100/2200
+	Health:  48/96/192 
+	DPS:   3/3/3 
+	Attack_Range:  0/0/0 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Slardar 
+	type1:   Scaled 
+	type2:   Warrior 
+	type3:  650/1300/2600
+	Health:   71/142/283 
+	DPS:   1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Timbersaw 
+	type1:   Scrappy 
+	type2:  Inventor 
+	Health:  800/1600/3200 
+	DPS:   41/82/164 
+	Attack_Range:   1/1/1 
+	Armor:   6/8/10 
+}, 
+{
+	name:  Treant Protector 
+	type1:   Elusive 
+	type2:  Druid 
+	Health:  650/1300/2300 
+	DPS:   38/75/150 
+	Attack_Range:   1/1/1 
+	Armor:  5/5/5 
+}, 
+{
+	name:  Witch Doctor 
+	type1:   Troll 
+	type2:  Warlock 
+	type3:  550/1100/2200
+	Health:  32/64/129 
+	DPS:  3/3/3 
+	Attack_Range:  5/5/5 
+	Armor:  0/0/0 
+}, 
+{
+	name:  Abaddon 
+	type1:   Heartless 
+	type2:   Knight 
+	Health:  750/1500/3000 
+	DPS:  38/77/154 
+	Attack_Range:   1/1/1 
+	Armor:   10/10/10 
+}, 
+{
+	name:  Arc Warden 
+	type1:  Primordial 
+	type2:   Shaman 
+	type3:  600/1100/2200
+	Health:  47/94/159 
+	DPS:   4/4/4 
+	Attack_Range:  5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Lina 
+	type1:  Human 
+	type2:  Mage 
+	type3:  550/1100/2200
+	Health:  33/66/131 
+	DPS:   4/4/4 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Lycan 
+	type1:   Human 
+	type2:  Savage 
+	type3: Warrior
+	Health:   750/1500/3000 
+	DPS:   50/100/200 
+	Attack_Range:  1/1/1 
+	Armor:   5/5/5 
+}, 
+{
+	name:  Omniknight 
+	type1:  Human 
+	type2:   Knight 
+	Health:  700/1400/2800 
+	DPS:  39/79/157 
+	Attack_Range:   1/1/1 
+	Armor:  10/10/10 
+}, 
+{
+	name:  Phantom Assassin 
+	type1:   Elusive 
+	type2:  Assassin 
+	type3:  550/1100/2200
+	Health:   67/133/267 
+	DPS:   1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Razor 
+	type1:   Primordial 
+	type2:   Mage 
+	type3:  750/1500/3000
+	Health:  50/100/200 
+	DPS:  3/3/3 
+	Attack_Range:  5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Sand King 
+	type1:   Savage 
+	type2:   Assassin 
+	type3:  550/1100/2200
+	Health:  38/75/150 
+	DPS:   1/1/1 
+	Attack_Range:   10/10/10 
+	Armor:   5/5/5 
+}, 
+{
+	name:  Shadow Fiend 
+	type1:   Demon 
+	type2:  Warlock 
+	type3:  450/900/1800
+	Health:   68/136/273 
+	DPS:  4/4/4 
+	Attack_Range:  5/5/5 
+	Armor:  0/0/0 
+}, 
+{
+	name:  Slark 
+	type1:   Scaled 
+	type2:  Assassin 
+	type3:  500/1000/2000
+	Health:  55/102/208 
+	DPS:  1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:   5/5/5 
+}, 
+{
+	name:  Sniper 
+	type1:   Deadeye 
+	type2:  Hunter 
+	type3:  450/900/1800
+	Health:  86/122/216 
+	DPS:   7/7/7 
+	Attack_Range:  5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Terrorblade 
+	type1:   Demon 
+	type2:   Demon Hunter 
+	type3:  650/1300/2600
+	Health:  39/79/157 
+	DPS:   1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Venomancer 
+	type1:   Savage 
+	type2:  Warlock 
+	type3:  500/1000/2000
+	Health:   46/92/183 
+	DPS:   3/3/3 
+	Attack_Range:   0/0/0 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Viper 
+	type1:   Dragon 
+	type2:  Assassin 
+	type3:  550/1100/2200
+	Health:  55/110/195 
+	DPS:   3/3/3 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Wind Ranger 
+	type1:   Elusive 
+	type2:   Hunter 
+	type3:  550/1100/2200
+	Health:   69/139/278 
+	DPS:   4/4/4 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Alchemist 
+	type1:   Scrappy 
+	type2:   Warlock 
+	Health:  1000/2000/4000 
+	DPS:   60/120/240 
+	Attack_Range:  1/1/1 
+	Armor:   5/5/5 
+}, 
+{
+	name:  Disruptor 
+	type1:  Brawny 
+	type2:   Shaman 
+	type3:  700/1400/2800
+	Health:  32/63/127 
+	DPS:   4/4/4 
+	Attack_Range:  5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Doom 
+	type1:   Demon 
+	type2:  Warrior 
+	Health:  1050/2100/4200 
+	DPS:   88/177/354 
+	Attack_Range:   1/1/1 
+	Armor:   5/5/5 
+}, 
+{
+	name:  Dragon Knight 
+	type1:   Human 
+	type2:   Dragon 
+	type3: Knight
+	Health:   800/1600/3200 
+	DPS:  46/92/183 
+	Attack_Range:  1/1/1 
+	Armor:  10/10/10 
+}, 
+{
+	name:  Keeper of the Light 
+	type1:  Human 
+	type2:   Mage 
+	Health:  650/1300/2600 
+	DPS:  25/50/100 
+	Attack_Range:   4/4/4 
+	Armor:  5/5/5 
+}, 
+{
+	name:  Kunkka 
+	type1:  Human 
+	type2:   Warrior 
+	Health:  950/1900/3800 
+	DPS:  59/118/236 
+	Attack_Range:  1/1/1 
+	Armor:  10/10/10 
+}, 
+{
+	name:  Lone Druid 
+	type1:  Savage 
+	type2:   Druid 
+	type3:  800/1600/3000
+	Health:   42/85/169 
+	DPS:  3/3/3 
+	Attack_Range:  0/0/0 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Medusa 
+	type1:   Scaled 
+	type2:  Hunter 
+	Health:  850/1700/3300 
+	DPS:  55/110/220 
+	Attack_Range:   4/4/4 
+	Armor:  5/5/5 
+}, 
+{
+	name:  Mirana 
+	type1:   Elusive 
+	type2:   Hunter 
+	Health:  750/1500/3000 
+	DPS:  60/120/240 
+	Attack_Range:   4/4/4 
+	Armor:   5/5/5 
+}, 
+{
+	name:  Necrophos 
+	type1:  Heartless 
+	type2:   Warlock 
+	Health:  850/1700/3400 
+	DPS:   46/92/183 
+	Attack_Range:  3/3/3 
+	Armor:   5/5/5 
+}, 
+{
+	name:  Templar Assassin 
+	type1:   Elusive 
+	type2:   Assassin 
+	type3:  750/1500/3000
+	Health:   83/167/333 
+	DPS:   3/3/3 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Troll Warlord 
+	type1:   Troll 
+	type2:   Warrior 
+	type3:  900/1800/3600
+	Health:  86/173/345 
+	DPS:   3/3/3 
+	Attack_Range:   5/5/5 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Enigma 
+	type1:  Primordial 
+	type2:  Warlock 
+	Health:  1000/2000/4000 
+	DPS:   33/67/133 
+	Attack_Range:   3/3/3 
+	Armor:   5/5/5 
+}, 
+{
+	name:  Gyrocopter 
+	type1:   Deadeye 
+	type2:  Inventor 
+	type3:  900/1800/3600
+	Health:   86/172/344 
+	DPS:   6/6/6 
+	Attack_Range:   15/15/15 
+	Armor:   0/0/0 
+}, 
+{
+	name:  Lich 
+	type1:  Heartless 
+	type2:  Mage 
+	type3:  850/1700/3400
+	Health:  28/57/133 
+	DPS:   3/3/3 
+	Attack_Range:  5/5/5 
+	Armor:   5/5/5 
+}, 
+{
+	name:  Techies 
+	type1:   Scrappy 
+	type2:   Inventor 
+	Health:  1000/2000/4000 
+	DPS:   33/67/133 
+	Attack_Range:   4/4/4 
+	Armor:   10/10/10 
+}, 
+{
+	name:  Tidehunter 
+	type1:  Scaled 
+	type2:  Hunter 
+	type3:  950/1900/3800
+	Health:  25/50/100 
+	DPS:   1/1/1 
+	Attack_Range:   5/5/5 
+	Armor:   5/5/5 
+}, 
