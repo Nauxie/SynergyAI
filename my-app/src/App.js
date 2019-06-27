7e1a7440ffca531968d0249c6a659237ce75c1e0
Nauxie <abhinav.tumu1@gmail.com>
2019-06-26 17:52:14 -0700

    cleanup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,28 +2,17 @@ import React from 'react';
 import HeroCard from './components/HeroCard'
 import './App.css';
 import hlist from './data/Heroes'
-
-
-
-
-
-
-
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       search: ''
     }
-
-
   }
   updateSearch(event) {
     this.setState({ search: event.target.value });
   }
-
   render() {
-
     let filteredhlist = hlist.filter(
       (hero) => {
         return hero.name.toLowerCase().includes(this.state.search.toLowerCase())
@@ -32,29 +21,19 @@ class App extends React.Component {
     const list = filteredhlist.map(list => {
       return (<HeroCard hero={list} key={list.id} />)
     })
-
-
-
-
     return (
       <div className="page">
         <div className="namecenter">
           <a href="https://underlords.com/">
-
             <img src="https://cdn-www.bluestacks.com/bs-images/Logo508.png" alt="logo" width="250px" />
           </a>
           <div className="searchbar">
             <input type="text" placeholder="Search..." value={this.state.search} onChange={this.updateSearch.bind(this)} />
           </div>
         </div>
-
         {list}
-
       </div>
     )
-
   }
-
 }
-
 export default App;
